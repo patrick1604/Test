6f071d000649b8467ad62e72f6a180705b2f0fe3
mstrasser92 <m.strasser@student.tugraz.at>
2014-03-26 12:48:34 +0100

    neue master

---
+++ AppzweiTest/src/com/example/appzwei/test/TestClass.java
@@ -0,0 +1,11 @@
+package com.example.appzwei.test;
+
+import com.example.appzwei.MainActivity;
+import com.robotium.solo.Solo;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TestClass extends ActivityInstrumentationTestCase2<MainActivity> {
+
+
+
+}
